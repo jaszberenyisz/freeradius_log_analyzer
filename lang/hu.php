a2cdc657b7ccc2b376e701fde9f6f90c1cba5b57
Szabolcs Jaszberenyi <jszabolcs2006@gmail.com>
2024-01-15 14:11:20 +0100

    Create hu.php
    Hungarian language file

---
+++ lang/hu.php
@@ -0,0 +1,12 @@
+<?php
+  define("_generated_at","Generálás ideje");
+  define("_home_directory_not_found","NINCS home könyvtár");
+  define("_mac_addresses","MAC címek");
+  define("_pcs","db");
+  define("_unique","egyedi");
+  define("_user","Felhasználó");
+  define("_uses_more_mac_addresses_than_treshold_value","több MAC címet használ, mint");
+  define("_vpn","VPN");
+  define("_warning","Figyelem");
+  define("_wifi","WIFI");
+?>
