0988ea05de8f6b13aa2d2d49ec65ccd99745f7fd
Szabolcs Jaszberenyi <jszabolcs2006@gmail.com>
2024-01-15 14:10:45 +0100

    Create en.php
    English language file

---
+++ lang/en.php
@@ -0,0 +1,12 @@
+<?php
+  define("_generated_at","Generated at");
+  define("_home_directory_not_found","Home directory NOT found");
+  define("_mac_addresses","MAC addresses");
+  define("_pcs","pcs");
+  define("_unique","unique");
+  define("_uses_more_mac_addresses_than_treshold_value","uses more MAC addresses than treshold value");
+  define("_user","User");
+  define("_vpn","VPN");
+  define("_warning","Warning");
+  define("_wifi","WIFI");
+?>
